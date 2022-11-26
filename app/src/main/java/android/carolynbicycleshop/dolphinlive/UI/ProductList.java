2c73863a0e1358d4a84fbab0bd8121101f9101af
carolyn.sher <carolyn.sher@wgu.edu>
2022-11-26 16:24:49 -0500

    Initial commit

---
+++ app/src/main/java/android/carolynbicycleshop/dolphinlive/UI/ProductList.java
@@ -0,0 +1,15 @@
+package android.carolynbicycleshop.dolphinlive.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.carolynbicycleshop.dolphinlive.R;
+import android.os.Bundle;
+
+public class ProductList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+    }
+}
