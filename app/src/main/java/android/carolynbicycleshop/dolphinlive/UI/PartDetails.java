f8c1d546303d5c2f93e8bcb7810b24e913ca4467
carolyn.sher <carolyn.sher@wgu.edu>
2022-11-26 16:47:17 -0500

    Part 1 complete

---
+++ app/src/main/java/android/carolynbicycleshop/dolphinlive/UI/PartDetails.java
@@ -0,0 +1,15 @@
+package android.carolynbicycleshop.dolphinlive.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.carolynbicycleshop.dolphinlive.R;
+import android.os.Bundle;
+
+public class PartDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part_details);
+    }
+}
