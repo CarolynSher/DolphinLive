5d50955098a04aac331ec6ffc8c83508c9590c2f
carolyn.sher <carolyn.sher@wgu.edu>
2022-12-10 19:48:56 -0500

    Part 3 end of session

--- app/src/main/java/android/carolynbicycleshop/dolphinlive/UI/PartDetails.java
+++ app/src/main/java/android/carolynbicycleshop/dolphinlive/UI/PartDetails.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.carolynbicycleshop.dolphinlive.R;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class PartDetails extends AppCompatActivity {
-
+    EditText editName;
+    EditText editPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part_details);
+
     }
 }
--- app/src/main/java/android/carolynbicycleshop/dolphinlive/UI/ProductList.java
+++ app/src/main/java/android/carolynbicycleshop/dolphinlive/UI/ProductList.java
@@ -1,21 +1,38 @@
 package android.carolynbicycleshop.dolphinlive.UI;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.carolynbicycleshop.dolphinlive.R.*;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.carolynbicycleshop.dolphinlive.Database.Repository;
 import android.carolynbicycleshop.dolphinlive.R;
+import android.carolynbicycleshop.dolphinlive.entities.Product;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 public class ProductList extends AppCompatActivity {
+    private Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_list);
-        FloatingActionButton fab=findViewById(R.id.floatingActionButton);
+        setContentView(layout.activity_product_list);
+        RecyclerView recyclerView = findViewById(id.productrecyclerview);
+        final ProductAdapter productAdapter = new ProductAdapter(this);
+        recyclerView.setAdapter(productAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        repository = new Repository(getApplication());
+        List<Product> allProducts = repository.getAllProducts();
+        FloatingActionButton fab = findViewById(id.floatingActionButton);
+        productAdapter.setProducts(allProducts);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,5 +40,19 @@ public class ProductList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        List<Product> allProducts = repository.getAllProducts();
+        RecyclerView recyclerView = findViewById(R.id.productrecyclerview);
+        final ProductAdapter productAdapter = new ProductAdapter(this);
+        recyclerView.setAdapter(productAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        productAdapter.setProducts(allProducts);
+
+        //Toast.makeText(ProductDetails.this,"refresh list",Toast.LENGTH_LONG).show();
     }
 }
