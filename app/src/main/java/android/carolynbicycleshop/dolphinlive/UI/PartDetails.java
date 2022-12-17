ce536e3bab23aa82e256a244e6c52aa017a61ff5
carolyn.sher <carolyn.sher@wgu.edu>
2022-12-17 10:36:12 -0500

    Prep code for Part 4

--- app/src/main/java/android/carolynbicycleshop/dolphinlive/UI/PartDetails.java
+++ app/src/main/java/android/carolynbicycleshop/dolphinlive/UI/PartDetails.java
@@ -2,20 +2,36 @@ package android.carolynbicycleshop.dolphinlive.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.PendingIntent;
 import android.carolynbicycleshop.dolphinlive.Database.Repository;
 import android.carolynbicycleshop.dolphinlive.R;
 import android.carolynbicycleshop.dolphinlive.entities.Part;
 import android.carolynbicycleshop.dolphinlive.entities.Product;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class PartDetails extends AppCompatActivity {
     EditText editName;
     EditText editPrice;
+    EditText editDate;
+    EditText editNote;
+    DatePickerDialog.OnDateSetListener startDate;
+    final Calendar myCalendarStart = Calendar.getInstance();
     String name;
     double price;
     int id;
@@ -29,6 +45,11 @@ public class PartDetails extends AppCompatActivity {
         setContentView(R.layout.activity_part_details);
         editName = findViewById(R.id.partname);
         editPrice = findViewById(R.id.partprice);
+        editDate = findViewById(R.id.editdate);
+        editNote = findViewById(R.id.editnote);
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        editDate.setText(sdf.format(new Date()));
         id = getIntent().getIntExtra("id", -1);
         name = getIntent().getStringExtra("name");
         price = getIntent().getDoubleExtra("price", -1.0);
@@ -51,7 +72,50 @@ public class PartDetails extends AppCompatActivity {
                 }
             }
         });
+        editDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Date date;
+                //get value from other screen,but I'm going to hard code it right now
+                String info=editDate.getText().toString();
+                if(info.equals(""))info="02/10/22";
+                try{
+                    myCalendarStart.setTime(sdf.parse(info));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                new DatePickerDialog(PartDetails.this, startDate, myCalendarStart
+                        .get(Calendar.YEAR), myCalendarStart.get(Calendar.MONTH),
+                        myCalendarStart.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        startDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+
+                myCalendarStart.set(Calendar.YEAR, year);
+                myCalendarStart.set(Calendar.MONTH, monthOfYear);
+                myCalendarStart.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+
+                updateLabelStart();
+            }
+
+        };
     }
+    private void updateLabelStart() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editDate.setText(sdf.format(myCalendarStart.getTime()));
+    }
+
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_partdetails, menu);
@@ -62,6 +126,34 @@ public class PartDetails extends AppCompatActivity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 this.finish();
+                return true;
+            case R.id.share:
+                Intent sendIntent=new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,editNote.getText().toString());
+                sendIntent.putExtra(Intent.EXTRA_TITLE,"Message Title");
+                sendIntent.setType("text/plain");
+                Intent shareIntent=Intent.createChooser(sendIntent,null);
+                startActivity(shareIntent);
+                return true;
+            case R.id.notifystart:
+                String dateFromScreen=editDate.getText().toString();
+                String myFormat = "MM/dd/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                Date myDate=null;
+                try {
+                    myDate=sdf.parse(dateFromScreen);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                Long trigger=myDate.getTime();
+                /*Intent intent= new Intent(PartDetails.this,MyReceiver.class);
+                intent.putExtra("key","message I want to see");
+                PendingIntent sender=PendingIntent.getBroadcast(PartDetails.this, ++MainActivity.numAlert,intent,PendingIntent.FLAG_IMMUTABLE);
+                AlarmManager alarmManager=(AlarmManager)getSystemService(Context.ALARM_SERVICE);
+                alarmManager.set(AlarmManager.RTC_WAKEUP, trigger, sender);*/
+                return true;
+            case R.id.notifyend:
                 return true;
         }
         return super.onOptionsItemSelected(item);
